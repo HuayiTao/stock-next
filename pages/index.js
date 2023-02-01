1f12211cf31648d1a32a1525e113a64456028676
HuayiTao <119065234+HuayiTao@users.noreply.github.com>
2023-02-01 15:15:58 +0700

    Add my name

--- pages/index.js
+++ pages/index.js
@@ -17,8 +17,7 @@ export default function Home() {
       <main className={styles.main}>
         <div className={styles.description}>
           <p>
-            Get started by editing&nbsp;
-            <code className={styles.code}>pages/index.js</code>
+            Huayi Tao
           </p>
           <div>
             <a
