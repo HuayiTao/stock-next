7e23ad9e9cd4eb996ed730e4005b773f7896bbb2
HuayiTao <119065234+HuayiTao@users.noreply.github.com>
2023-02-16 22:50:10 +0700

    Init

---
+++ pages/api/customer/product/index.js
@@ -0,0 +1,31 @@
+import {connect, model, models, Schema} from "mongoose"
+const connectionString = 'mongodb+srv://root:thy990922@cluster0.3vb3cx0.mongodb.net/customer'
+
+export default async function handler(req, res) {
+    await connect(connectionString);
+    console.log("req.method: ", req.method)
+
+    if (req.method === 'GET') {
+        const docs = await Product.find()
+        res.status(200).json(docs)
+    } else if (req.method === 'POST') {
+        console.log(req.body)
+        // res.status(200).json(req.body)
+        const doc = await Product.create(req.body)
+        res.status(201).json(doc)
+    } else {
+        res.setHeader('Allow', ['GET', 'POST'])
+        res.status(405).end(`Method ${req.method} Not Allowed`)
+    }
+}
+
+
+
+const productSchema = new Schema({
+    Code: String,
+    Name: String,
+    Price: String,
+});
+
+console.log("Mongoose Models", models)
+const Product = models?.product || model('product', productSchema);
